c715f87ca932a9f96fc0cb037114695fa0ede9c8
bulbrain <georgi_d_g1@abv.bg>
2015-04-16 18:21:33 +0200

    Fixed old homework

--- Programming/0.C#/ConditionalStatement/10. BeerTime/BeerTime.cs
+++ Programming/0.C#/ConditionalStatement/10. BeerTime/BeerTime.cs
@@ -1,11 +1,19 @@
-﻿using System;
+using System;
 
 class BeerTime
 {
     static void Main()
     {
         string time = Console.ReadLine();
-        DateTime dt =
-        DateTime.ParseExact(time, "yyyy-MM-dd HH:mm");
+        DateTime dt = DateTime.Parse(time);
+        if (dt.Hour >= 13 || dt.Hour <= 3)
+        {
+            Console.WriteLine("Beer time.");
+        }
+        else
+        {
+            Console.WriteLine("No beer time.");
+        }
+        Console.ReadLine();
     }
 }
